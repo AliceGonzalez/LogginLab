4df90f0519623b2e89015671655e2427d81c9fc8
AliceGonzalez <alicegcodes@gmail.com>
2024-03-07 12:10:49 -0500

    kris adjust

--- src/main/java/LogginLab.java
+++ src/main/java/LogginLab.java
@@ -38,10 +38,10 @@ public class LogginLab {
     // Write a method called thresholdReached, returns true if argument 'limit' is over the threshold.
     // use thresholdExceeds for a pattern.
     // Write a test for the method in the Test class.
-    public boolean thresholdReached{
-        if (threshold > thresholdExceeds(
-                return true;
-        )
+    public boolean thresholdReached(Integer limit) {
+        if (limit > this.threshold) {
+            return true;
+        }
         return false;
     }
 }
